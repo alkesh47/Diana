12676c7957c0545e766bc7f62ac288cd925f3b82
Alkesh <alkeshsrivastava11@gmail.com>
2016-07-20 03:38:51 +0530

    The Database is now working but still needs a few major tweaks.

--- app/src/main/java/com/example/alkesh/expensemanager101/MainActivity.java
+++ app/src/main/java/com/example/alkesh/expensemanager101/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.alkesh.expensemanager101;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
 
     @Override
