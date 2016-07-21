0e7c6d9fc2c56422b48b84ad78d6f6c083daeb84
Alkesh <alkeshsrivastava11@gmail.com>
2016-07-21 18:33:27 +0530

    Date Picker dialog successfully integrated.

--- app/src/main/java/com/example/alkesh/expensemanager101/AddMoney.java
+++ app/src/main/java/com/example/alkesh/expensemanager101/AddMoney.java
@@ -2,7 +2,6 @@ package com.example.alkesh.expensemanager101;
 
 import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,15 +10,16 @@ import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 
+@TargetApi(Build.VERSION_CODES.N)
 public class AddMoney extends AppCompatActivity implements View.OnClickListener{
 
     Button Save,Cancel;
@@ -27,17 +27,46 @@ public class AddMoney extends AppCompatActivity implements View.OnClickListener{
 
     private SQLiteDatabase db;
 
+    Calendar myCalendar = Calendar.getInstance();
+
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_money);
 
-        OpenDatabase();
-
-
         Date=(EditText)findViewById(R.id.dateEditText);
         Notes=(EditText)findViewById(R.id.notesEditText);
         expText=(EditText)findViewById(R.id.expense);
+
+
+
+         final DatePickerDialog.OnDateSetListener datePickerDialog = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR,i);
+                myCalendar.set(Calendar.MONTH,i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH,i2);
+                updateLabel();
+
+            }
+        };
+
+        Date.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    new DatePickerDialog(AddMoney.this, (DatePickerDialog.OnDateSetListener) datePickerDialog,myCalendar.get(Calendar.YEAR),
+                            myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+                return false;
+            }
+        });
+
+
+        OpenDatabase();
+
 
         Cancel=(Button)findViewById(R.id.cancelbutton);
         Cancel.setOnClickListener(this);
@@ -45,6 +74,14 @@ public class AddMoney extends AppCompatActivity implements View.OnClickListener{
         Save = (Button)findViewById(R.id.savebutton);
         Save.setOnClickListener(this);
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private void updateLabel(){
+        String myFormat = "dd-MM-yyyy";
+
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat(myFormat,Locale.US);
+        Date.setText(String.valueOf(simpleDateFormat.format(myCalendar.getTime())));
     }
 
 
