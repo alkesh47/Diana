17409328263e3d5876b73949c139a305f699d9ec
Alkesh <alkeshsrivastava11@gmail.com>
2016-07-23 17:42:31 +0530

    Work is still left on the database segment.

---
+++ app/src/main/java/com/example/alkesh/expensemanager101/DBOverview.java
@@ -0,0 +1,13 @@
+package com.example.alkesh.expensemanager101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DBOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dboverview);
+    }
+}
